29c8a88f02db1ae2572768036ceea329c73e6b01
jahidulkanchan <mdj524156@gmail.com>
2023-10-19 12:44:28 +0600

    new add

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ toogleBar.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
+
+// Owl carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  dots:false,
+  nav:true,
+  autoplay:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
